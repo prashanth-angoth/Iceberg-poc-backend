9d1a500da60cd84078b3e1ec9420a9f7beab7a7c
unknown <angodhprashanth@gmail.com>
2025-03-25 00:44:37 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const peopleContrll = require("./controller.js");
+
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+app.post("/api/v1/people/create", peopleContrll.createPeople);
+app.post("/api/v1/people/insert",peopleContrll.insertData)
+app.get("/api/v1/people/getPeoples", peopleContrll.getPeoples);
+app.put("/api/v1/people/update/:id", peopleContrll.updatePeople);
+app.delete("/api/v1/people/delete/:id", peopleContrll.deletePeople);
+app.post("/api/v1/people/bulkDelete", peopleContrll.bulkDelete);
+app.post("/api/v1/people/bulkUpdate", peopleContrll.bulkUpdate);
+ 
+
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
